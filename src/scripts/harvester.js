ac60441b58b3889e0fc2f2f5934b8a114b1b50ad
Cody Clark <codyjamesclark@gmail.com>
2024-06-04 13:43:11 -0500

    fixed typo in harvester module

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,8 +1,8 @@
 export const harvestPlants = (plants) => {
   let seedOutput = [];
   for (const plant of plants) {
-    if (plant.type === 'corn') {
-      const cornOutput = plant.output / 2;
+    if (plant.type === 'Corn') {
+      const cornOutput = (plant.output / 2);
       for (let i = 0; i < cornOutput; i++) {
         seedOutput.push(plant);
       }

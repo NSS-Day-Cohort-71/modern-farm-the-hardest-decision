52ad5112040c3ab6e1ab05c881b34d51b84bed28
Cody Clark <codyjamesclark@gmail.com>
2024-06-04 10:02:48 -0500

    added function to create the corn seed

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,4 +1,10 @@
 console.log("Welcome to the main module");
 import { createPlan } from "./plan.js";
+import { createCorn } from "./seeds/corn.js";
 
 const yearlyPlan = createPlan();
+
+
+const cornSeed = createCorn()
+
+console.log(cornSeed)

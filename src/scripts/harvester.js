9ae74f7488e174759482dce7b523a1aa61b7a504
Zach Mosher <zmosher4@gmail.com>
2024-06-04 14:12:35 -0500

    clean up

--- src/scripts/harvester.js
+++ src/scripts/harvester.js
@@ -1,18 +1,14 @@
 export const harvestPlants = (plants) => {
   let seedOutput = [];
   for (const plant of plants) {
-
-    if (plant.type === "Corn") {
-
+    if (plant.type === 'Corn') {
       const cornOutput = plant.output / 2;
       for (let i = 0; i < cornOutput; i++) {
         seedOutput.push(plant);
       }
     } else {
-      for (let i = 0; i < plant.output; i++) 
-        seedOutput.push(plant);
-      }
+      for (let i = 0; i < plant.output; i++) seedOutput.push(plant);
     }
   }
-  return seedOutput;
 };
+return seedOutput;

3e06dc3da69975bf5d96db05ffebe4e9f328f401
Michael Galo <michael@michaelthinks.co>
2024-06-04 10:05:57 -0500

    added fields.js module

---
+++ src/scripts/field.js
@@ -0,0 +1,21 @@
+const growingPlants = [];
+
+// use the Array.isArray method to see if corn gets passed in.
+// most seed objs will be fine, but I need to make a forof loop likely
+
+export const addPlant = (seedObj) => {
+    if (Array.isArray(obj) === true){
+        growingPlants.push(seedObj)
+    } else (Array.isArray(obj) === false) {
+        for (const corn of seedObj) {
+            growingPlants.push(corn)
+        }
+    }
+};
+
+//map the growingPlants array below using the .Map method and export it
+
+export const usePlants = () => {
+    const growingPlantsCopy = growingPlants.map((obj) => ({...obj}))
+    return growingPlantsCopy
+};

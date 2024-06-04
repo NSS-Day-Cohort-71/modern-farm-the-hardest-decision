45208285c499b45f33eaac5e3124d8fb41628e09
Michael Galo <michael@michaelthinks.co>
2024-06-04 10:27:43 -0500

    fields.js hotfixed and corn function imported to main.js

--- src/scripts/field.js
+++ src/scripts/field.js
@@ -1,21 +1,16 @@
 const growingPlants = [];
 
-// use the Array.isArray method to see if corn gets passed in.
-// most seed objs will be fine, but I need to make a forof loop likely
-
 export const addPlant = (seedObj) => {
-    if (Array.isArray(obj) === true){
-        growingPlants.push(seedObj)
-    } else (Array.isArray(obj) === false) {
-        for (const corn of seedObj) {
-            growingPlants.push(corn)
-        }
+  if (Array.isArray(seedObj) === false) {
+    growingPlants.push(seedObj);
+  } else if (Array.isArray(seedObj) === true) {
+    for (const corn of seedObj) {
+      growingPlants.push(corn);
     }
+  }
 };
 
-//map the growingPlants array below using the .Map method and export it
-
 export const usePlants = () => {
-    const growingPlantsCopy = growingPlants.map((obj) => ({...obj}))
-    return growingPlantsCopy
+  const growingPlantsCopy = growingPlants.map((obj) => ({ ...obj }));
+  return growingPlantsCopy;
 };

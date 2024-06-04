6dd0bba28c828192375b1d0d5cfe59a2ad9bfbec
Cody Clark <codyjamesclark@gmail.com>
2024-06-04 15:36:34 -0500

    deleted unecessary imports

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,7 @@
 console.log("Welcome to the main module");
 import { Catalog } from "./catalog.js";
-import { usePlants } from "./field.js";
 import { harvestPlants } from "./harvester.js";
 import { createPlan } from "./plan.js";
-import { createCorn } from "./seeds/corn.js";
 import { plantSeeds } from "./tractor.js";
 
 const yearlyPlan = createPlan();
@@ -11,14 +9,7 @@ const yearlyPlan = createPlan();
 const plantedSeeds = plantSeeds(yearlyPlan);
 
 const harvestedPlants = harvestPlants(plantedSeeds);
-harvestedPlants.sort((a, b) => {
-  if (a.type < b.type) {
-    return -1;
-  } else if (a.type > b.type) {
-    return 1;
-  }
-  return 0;
-});
+
 const plantsForSale = Catalog(harvestedPlants);
 
 const plantSale = document.querySelector(".container");

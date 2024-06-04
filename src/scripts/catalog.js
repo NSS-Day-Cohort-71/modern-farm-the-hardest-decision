af87cc6f46c7bc19dba949e39c57832e0208cbc1
Michael Galo <michael@michaelthinks.co>
2024-06-04 10:43:01 -0500

    catalog.js and Catalog function created

---
+++ src/scripts/catalog.js
@@ -0,0 +1,11 @@
+export const Catalog = (arr) => {
+  let Catalog = "";
+
+  for (const harvest of arr) {
+    Catalog += `
+        <section class="plant">${harvest.type}</section>
+        `;
+  }
+
+  return Catalog;
+};
